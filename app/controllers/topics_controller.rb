a181981315ebb3ca19c8df8f9fda63c53f32f4c3
Kyle D <kdornblaser@gmail.com>
2015-09-02 19:21:50 +0000

    Add has many association between users and topics.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,7 +4,7 @@ class TopicsController < ApplicationController
   # GET /topics
   # GET /topics.json
   def index
-    @topics = Topic.all
+    @topics = current_user.topics
   end
 
   # GET /topics/1
